9b8d1b714fd547aff08fee12e83d56d3d7186787
giladcohen <giladadlig@gmail.com>
2022-05-21 04:18:06 +0300

    u

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,5 +7,5 @@ from .eval import *
 
 # progress bar
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
-from progress.bar import Bar as Bar
+# sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
+# from progress.bar import Bar as Bar
